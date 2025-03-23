0435bac9d7bcf85eed51cdad1e2f43297925aeff
Faizan Shaikh <faizanshaikh008007@gmail.com>
2025-03-23 18:13:40 +0530

    First commit

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+import { PdfReader } from "@/features/pdf-reader";
+
+export default function Home() {
+  return (
+    <div className="min-h-screen bg-black">
+      <PdfReader />
+    </div>
+  );
+}
